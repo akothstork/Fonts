03b4b38223e40f4e591ea639b1c4b8bfcc42bd8b
akothstork18-sudo <akothstork18@gmail.com>
2026-07-11 17:19:22 +0200

    Add files via upload

---
+++ better-typing-indicators/manifest.js
@@ -0,0 +1,15 @@
+/** @type {import("@vtypes/plugin").PluginManifest} */
+export default {
+    name: "better-typing-indicators",
+    description: `better-typing-indicators.`,
+    authors: [
+        {
+            name: "sylv256",
+            id: "209015289990348800"
+        }
+    ],
+    main: "src/index.ts",
+    vendetta: {
+        icon: "ic_image"
+    }
+}
